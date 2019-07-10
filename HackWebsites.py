0eb2110d65cc99613e24a9bf6438be7a6e9318e9
MASTERX <50529518+seifsherifssss@users.noreply.github.com>
2019-07-11 00:36:52 +0200

    Add files via upload

---
+++ HackWebsites.py
@@ -0,0 +1,26 @@
+def hack () : 
+    import os
+    os.system("clear && toilet -f big 'HACKINGWEBSITES' | lolcat ")
+    import os
+    os.system("cd /$HOME && git clone https://github.com/Tuhinshubhra/RED_HAWK.git")
+    import os
+    os.system("cd /$HOME && git clone https://github.com/UndeadSec/SocialFish.git")
+    import os
+    os.system("cd /$HOME && git clone https://github.com/Bhai4You/Ip-Attack")
+    import os
+    os.system("cd /$HOME && git clone https://github.com/Bhai4You/Bull-Attack")
+    import os
+    os.system("cd /$HOME && git clone https://github.com/samyoyo/weeman")
+    import os
+    os.system("cd /$HOME && git clone https://github.com/j3ssie/Osmedeus")
+    import os
+    os.system("cd /$HOME && git clone https://github.com/radenvodka/Recsech")
+    import os
+    os.system("cd /$HOME && git clone https://github.com/s0md3v/Breacher.git")
+    import os
+    os.system("cd /$HOME && git clone https://github.com/bahaabdelwahed/killshot.git")
+    import os
+    os.system("cd /$HOME && git clone https://github.com/Gameye98/santet-online")
+    import os 
+    os.system("cd /$HOME && wget https://raw.githubusercontent.com/Hax4us/httrack_In_termux/master/httrack ")
+hack()
---
+++ requirements.sh
@@ -0,0 +1,5 @@
+pkg install toilet figlet curl php ruby nmap openssh openssl-tool nodejs ruby cowsay perl fish dnsutils termux-exec net-tools -y && pkg upgrade -y && gem install lolcat 
+
+clear
+
+toilet -f big 'DONE.....!' | lolcat 
